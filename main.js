01ef5ca52ee553e458a290bd94055b712c2245ba
thay20 <thay20@proton.me>
2022-10-27 20:03:29 -0400

    Create functions for player and computer choice

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log("Helllo World!");
+function getComputerChoice() {
+   let computerChoice = Math.floor(Math.random()*CHOICES.length);
+    return CHOICES[computerChoice];
+}
+
+function getPlayerChoice() {
+    let playerChoice = prompt('Will you choose RemotePlayback, Paper, or Scissors?').toUpperCase();
+    return playerChoice;
+}
+
+function playRound(playerSelection, computerSelection) {
+
+}
+
+const CHOICES = ['ROCK', 'PAPER', 'SCISSORS']
+
+let computerSelection = getComputerChoice();
+let playerSelection = getPlayerChoice();
+
+
+//console.log(computerSelection); //verify computerSelection is running correctly
+//console.log(playerSelection); //veriffy playSelection is running correctly
